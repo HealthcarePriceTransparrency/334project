c3d8f6ff2e011e2f48eb763bca2eabf208d9bc98
rche273 <130494281+rche273@users.noreply.github.com>
2023-12-06 19:33:09 -0500

    Updated dt.py with continuous y data, feature ranking

--- dt.py
+++ dt.py
@@ -5,6 +5,8 @@ import pandas as pd
 import time
 from sklearn import metrics
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+
 
 start = time.time()
 # reading already split data
@@ -17,27 +19,34 @@ xtrain = xtrain.to_numpy()
 xtest = xtest.to_numpy()
 ytrain = ytrain.to_numpy()
 ytest = ytest.to_numpy()
-yraveltrain = ytrain.ravel()
-ytrain = np.array(yraveltrain).astype(int)
-yraveltest = ytest.ravel()
-ytest = np.array(yraveltest).astype(int)
 
-tree = DecisionTreeRegressor()
+tree = DecisionTreeRegressor(criterion = "friedman_mse", min_samples_leaf=2, max_depth = 100)
 tree.fit(xtrain, ytrain)
 testPred = tree.predict(xtest)
 trainPred = tree.predict(xtrain)
-testAcc = metrics.accuracy_score(ytest, testPred)
-trainAcc = metrics.accuracy_score(ytrain, trainPred)
-testAuc = metrics.roc_auc_score(ytest, testPred)
-trainAuc = metrics.roc_auc_score(ytrain, trainPred)
+trainr2 = r2_score(trainPred, ytrain)
+testr2 = r2_score(testPred, ytest)
+
+print("Train R2: ", trainr2)
+print("Test R2: ", testr2)
+importances = tree.feature_importances_
+indices = np.argsort(importances)
+
+# graph of feature importance
+plt.title('Feature Importance')
+plt.barh(range(len(indices)), importances[indices], color='b', align='center')
+plt.yticks(range(len(indices)), ("npi_number","payer","city","state"))
+plt.xlabel('Relative Importance')
+plt.show()
 
 # graph of decision tree created
 plot_tree(tree, feature_names= ("npi_number","payer","city","state"), filled=True, rounded=True)
 timeElapsed = time.time() - start
 
 print("Time Elapsed: ", timeElapsed)
-print("Test Accuracy: ", testAcc)
-print("Train Accuracy: ", trainAcc)
+
+plt.show()
+
 print("Test AUC: ", testAuc)
 print("Train AUC: ", trainAuc)
 
